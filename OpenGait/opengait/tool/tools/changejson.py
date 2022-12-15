544170934c98a85d4e86a61080d46975b0c7e785
jdyjjj <1410234026@qq.com>
2022-12-15 15:24:53 +0800

    update

---
+++ OpenGait/opengait/tool/tools/changejson.py
@@ -0,0 +1,26 @@
+import json
+
+def writejson(path, idlist, gallery=True):
+    with open(path,'rb')as f:
+        jsondict = json.load(f)
+    list = []
+    for frame_id in idlist:
+        tid = '{:03d}'.format(frame_id)
+        list.append(tid)
+    if gallery:
+        jsondict['TEST_SET'] = list
+    else:
+        jsondict["COMPARE_SET"] = list
+    with open(path, 'w') as write_f:
+        json.dump(jsondict, write_f, indent=4, ensure_ascii=False)
+
+def getid(path, id):
+    with open(path,'rb')as f:
+        jsondict = json.load(f)
+    return jsondict['TEST_SET'][id-1]
+
+def getidlist(path, gallery):
+    with open(path,'rb')as f:
+        jsondict = json.load(f)
+    return jsondict['TEST_SET'] if gallery else jsondict['COMPARE_SET']
+    
---
+++ OpenGait/opengait/tool/tools/gait_compare.py
@@ -0,0 +1,26 @@
+import torch
+import numpy as np
+
+def getemb(data):
+    return data["inference_feat"]
+
+def computedistence(x, y):
+    distance = torch.sqrt(torch.sum(torch.square(x - y)))
+    return distance
+
+def compareid(data,dict,threshold_value):
+    embs = getemb(data)
+    min = threshold_value
+    id = None
+    for key in dict:
+        for subject in dict[key]:
+            for type in subject:
+                for view in subject[type]:
+                    value = subject[type][view]
+                    distance = computedistence(embs["embeddings"],value)
+                    if distance < min:
+                        id = key
+                        min = distance
+    if id is None:
+        print("############## no id #####################")
+    return id
